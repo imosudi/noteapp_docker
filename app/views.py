f8b3d3e1bc39093734aadae3c46b59e1de206fe2
Mosudi Isiaka <imosudi@gmail.com>
2020-05-20 06:06:32 +0100

    App UserRegistration Configuration

--- app/views.py
+++ app/views.py
@@ -13,6 +13,15 @@ from app import app
 
 from .models import *
 
+import mysql.connector as mariadb
+
+config = {
+    'host': '172.17.0.3',
+    'port': 3306,
+    'user': 'root',
+    'password': 'PASSWimosudi@gmail.co767868FFGFFDD#m',
+    'database': 'noteapp'
+}
 
 from datetime import datetime
 
@@ -35,12 +44,12 @@ def about():
     return render_template('about.html', pageName=pageName, current_time=datetime.utcnow())
     pass
 
-# User registration
+"""# User registration
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     pageName= 'register'
     return render_template('register.html', form=form, pageName=pageName, current_time=datetime.utcnow())
-
+"""
 # User login
 @main.route('/login', methods=['GET', 'POST'])
 def login():
@@ -80,6 +89,9 @@ def login():
 				error = 'Invalid login'
 				return render_template('login.html', pageName=pageName, form=form, current_time=datetime.utcnow(), error=error)
 
+			# Close database connection
+			cur.close()
+
 
 		else:
 			#app.logger.info('NO USER FOUND')
@@ -93,6 +105,42 @@ def login():
 def home():
 	return render_template("home.html", pageName=pageName, current_time=datetime.utcnow())
 	pass"""
+
+
+#User registration
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+	pageName = 'register'
+	form = registrationForm(request.form)
+	if request.method == 'POST' and  form.validate():
+		name = form.name.data
+		username = form.username.data
+		email = form.email.data
+		password = sha256_crypt.encrypt(str(form.password.data))
+
+		# create a connection cursor
+		conn = mariadb.connect(**config)
+
+		# login cursor 
+		cur = conn.cursor()
+
+		cur.execute("INSERT INTO users(name, username, email, password) VALUES(%s,	\
+	 %s, %s,%s)", (name, username, email, password))
+
+		# Commit to database
+		conn.connection.commit()
+
+		# Close database connection
+		cur.close()
+
+		flash(u"Registration Complete, you may proceed to login", "success")
+
+		return redirect(url_for('home'))
+
+	else:
+		return render_template('register.html', form=form, pageName=pageName,  current_time=datetime.utcnow())
+
+
 
 
 @app.errorhandler(404)
